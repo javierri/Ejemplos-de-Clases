ab43daf212c165eb04477578ba898e168a4d9c43
Javier Rivera Romero <javierri.rivera@gmail.com>
2016-04-02 19:23:16 -0430

    Create Ascensor.py

---
+++ Ascensor.py
@@ -0,0 +1,93 @@
+# Metodo de Clase Ascensor
+# Observación: Ver en https://repl.it/CBkF/0
+# Autor: Javier Rivera (UNEFA Mérida)
+
+
+class Ascensor:
+
+	# Atributos
+	encendido=False 
+	puerta_cerrada=False
+	cantidad_piso=10
+	numero_piso=0
+	capacidad=5
+	nro_personas=0
+	
+	# Métodos
+	def encender (self):
+		if (self.encendido==False):
+			self.encendido=True
+			
+	def apagar (self):
+		if (self.encendido==True):
+			self.encendido=False
+
+	def abrir_puerta(self):
+		if (self.encendido and self.puerta_cerrada==True):
+			self.puerta_cerrada=False
+			print "Abriendo puerta"
+
+	def cerrar_puerta(self):
+		if (self.encendido and self.puerta_cerrada==False):
+			self.puerta_cerrada=True
+			print "Cerrando puerta"
+
+	def subir (self):
+		if(self.puerta_cerrada and self.encendido):
+			if (self.numero_piso < self.cantidad_piso):
+				self.numero_piso = self.numero_piso+1
+				print "sube", self.numero_piso
+			else:
+				print "No puede subir, ultimo piso"
+		else:
+			print "Error al subir"
+
+	def bajar (self):
+		if(self.puerta_cerrada and self.encendido):
+			if (self.numero_piso > 0):
+				self.numero_piso = self.numero_piso-1
+				print "baja", self.numero_piso
+			else:
+				print "No puede bajar, ultimo piso"
+		else:
+			print "Error al bajar"
+	
+	def enviar (self, piso):
+		if (piso < 0) or (piso > self.cantidad_piso):
+			print "Error al enviar a piso"
+			return
+		
+		if (self.nro_personas > self.capacidad):
+			print "Exceso de personas"
+			return
+		
+		self.cerrar_puerta()
+		
+		if (self.numero_piso < piso):
+			for i in range (piso-self.numero_piso):
+				self.subir()
+				
+		if (self.numero_piso > piso):
+			for i in range (self.numero_piso-piso):
+				self.bajar()
+				
+		self.abrir_puerta()
+		
+	def llamar(self, piso):
+		pass
+	
+	def ingresa_persona(self):
+		pass
+	
+	def sale_persona(self):
+		pass
+		
+
+#PRINCIPAL
+
+a = Ascensor()
+a.encender()
+
+# PISOS
+a.enviar (7)
+a.enviar (3)
