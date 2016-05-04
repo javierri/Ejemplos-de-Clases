2a4ad915f9727d589279fda8b19614c7dc44e89f
Javier Rivera Romero <javierri.rivera@gmail.com>
2016-05-03 20:56:03 -0430

    Update Gato.py

--- Gato.py
+++ Gato.py
@@ -5,6 +5,11 @@ class Gato:
         self.__raza = raza
         self.__vivo = True
         self.__sexo = sexo
+        
+        self.__embarazada = False
+        self.__sem_gestacion = 0
+        self.__genes_padre = None
+        
         if (peso > 0 and peso < 0.5):
             self.__peso = peso
         else:
@@ -26,6 +31,49 @@ class Gato:
             
     def edad(self):
         return self.__edad
+        
+    def cita(self, pareja):
+		if (self.__sexo == pareja.__sexo):
+			return
+		
+		if (self.__sexo == "F" and self.__embarazada == False):
+			self.__embarazada = True
+			self.__sem_gestacion = 0
+			self.__genes_padre = pareja
+		elif (pareja.__sexo == "F" and pareja.__embarazada == False) :
+			pareja.__embarazada = True
+			pareja.__sem_gestacion = 0
+			pareja.__genes_padre = self
+			
+		print "miauuuu"
+		
+    def embarazada(self):
+		return self.__embarazada
+		
+    def cumple_sem_gestacion (self):
+		if (self.__embarazada == True):
+			self.__sem_gestacion = self.__sem_gestacion + 1
+			
+    def parir(self):
+		genes = (self,self.__genes_padre)
+		gaticos = []
+		
+		if (self.__embarazada == True and self.__sem_gestacion >= 9):
+			nro_hijos = random.randint(2, 5)
+	
+			for i in range(nro_hijos):
+				# Crea gato con genes aleatorios de los padres
+				h_raza = random.randint(0, 1) # Herencia Raza
+				h_color = random.randint(0, 1) # Herencia Color
+				h_sexo = random.randint(0, 1) # Herencia Sexo
+				gatico = Gato(genes[h_raza].__raza, genes[h_color].__color, genes[h_sexo].__sexo)
+				gaticos.append(gatico)
+				
+			self.__embarazada = False
+			self.__sem_gestacion = 0
+			self.__genes_padre = None
+
+		return gaticos
             
     def __str__(self):
         cad = self.__raza + ', ' + self.__color + ', ' + str(self.__sexo) + ', ' + str(self.__peso) 
@@ -38,3 +86,18 @@ peter = Gato ("Bengala","gris","M")
 
 print marie
 print peter
+
+peter.cita(marie)
+print "Embarazada: ", marie.embarazada()
+
+for i in range(9):
+	marie.cumple_sem_gestacion()
+	
+gaticos = marie.parir()
+
+print "Muestra hijos ..."
+for gatico in gaticos:
+	print gatico
+
+print "Embarazada: ", marie.embarazada()
+
